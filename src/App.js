069137f23809991ec1d2e73a297e9887c190c5b7
Keylington <ceo@keylingtonenterprises.com>
2026-04-05 16:30:53 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Services from './components/Services';
-import Portfolio from './components/Portfolio';
-import Team from './components/Team';
-import Testimonials from './components/Testimonials';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import './styles/App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Portfolio from "./components/Portfolio";
+import Team from "./components/Team";
+import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
-const App = () => {
-    return (
-        <div>
-            <Header />
-            <Hero />
-            <Services />
-            <Portfolio />
-            <Team />
-            <Testimonials />
-            <Contact />
-            <Footer />
-        </div>
-    );
-};
-
-export default App;
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Hero />
+      <Services />
+      <Portfolio />
+      <Team />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
