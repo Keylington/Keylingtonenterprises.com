a0d3b9060c45ec8f6969fc58cd9889f46c76c08a
Keylington <ceo@keylingtonenterprises.com>
2026-04-05 16:36:51 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
